284101fb8e63d31a00457c737c360547c6666c24
ocartaxo <oacartax@gmail.com>
2023-08-27 16:25:23 -0300

    default tests

---
+++ src/test/kotlin/br/com/kofood/eurekaserver/EurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.kofood.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
